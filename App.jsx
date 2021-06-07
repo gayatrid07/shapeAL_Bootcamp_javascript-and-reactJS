59afe589cf73fdf76d9c61978164759a9e188e30
gayatrid07 <85438332+gayatrid07@users.noreply.github.com>
2021-06-07 11:14:36 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+function app() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default app;
